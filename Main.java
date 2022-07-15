f67333e6354bbc6cd4ea81fafa45c10ad5592844
Deepak kumar sen <56968469+deepak1412@users.noreply.github.com>
2022-07-15 22:38:32 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+# BUS_Project_CipherSchools
+package BusProject;
+
+public class Main {
+
+     public static  void main(String[] args){
+
+          LonginSignup loginSignup = new LonginSignup();
+
+         System.out.println('longinSignup:' signup( name: "RAVI",phone 8957461662,email"deepakcsit14@gmail.com",street "xyz",state"abc",city"delhi"));
+         System.out.println(longinSignup.signup( name: "RAVI",phone 8957461662,email"deepakcsit14@gmail.com",street "xyz",state"abc",city"delhi"));
+         System.out.println(longinSignup.signup( name: "RAVI",phone 8957461662,email"deepakcsit14@gmail.com",street "xyz",state"abc",city"delhi"));
+          System.out.println(longinSignup.login( email: "deepakcsit14@gmai.com",a123456));
+
+
+
+
+
+     }
+
+}
